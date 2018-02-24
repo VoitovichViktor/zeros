daf81508149e4e7f720a612e7ae679b75f24db05
VoitovichViktor <bloknot.a6@gmail.com>
2018-02-24 10:07:19 +0300

    zeros: implemented solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var multiplier = 5;
+  var arr = [];
+  var result = 0;
+
+  while(arr[arr.length-1] >= 1 || arr.length == 0) {
+    arr.push(Math.floor(number / multiplier));
+    multiplier *= 5;
+  }
+
+  for (var i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  
+  return result;
 }
